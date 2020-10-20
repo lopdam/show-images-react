f842b43c27c0311737b7f709a90577e320897148
lopez damian <dennyslopez2013@gmail.com>
2020-10-20 02:40:38 -0500

    deploy 1

---
+++ precache-manifest.f85e9c88a3f6865d4e54ea239b4caf2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5fffcb80a70cc43d872e056613dfd3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1faebb17f36d02685a6",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "047895e1b2d687d118bf",
+    "url": "./static/css/main.5e7b09fb.chunk.css"
+  },
+  {
+    "revision": "c1faebb17f36d02685a6",
+    "url": "./static/js/2.fb3e287f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fb3e287f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047895e1b2d687d118bf",
+    "url": "./static/js/main.2ebc227f.chunk.js"
+  },
+  {
+    "revision": "ec27a66efc767a7790c4",
+    "url": "./static/js/runtime-main.906e6239.js"
+  }
+]);
